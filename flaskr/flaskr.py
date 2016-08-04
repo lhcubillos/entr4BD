5720c1dcb2c92fd696d0477e35507ca1262a4b28
mf222 <mfsepulveda@uc.cl>
2016-08-04 17:23:52 -0400

    Proyecto en blanco con conexion basica a mongo probada y conexion a postgresql incompleta

---
+++ flaskr/flaskr.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import psycopg2
+import pymongo
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+
+app = Flask(__name__)
+
+
+MONGODATABASE = "myDatabase"
+MONGOCOLLECTION = "myCollection"
+MONGOSERVER = "localhost"
+MONGOPORT = 27017
+client = MongoClient(MONGOSERVER, MONGOPORT)
+mongodb = client[MONGODATABASE]
+
+POSTGREDATABASE = "myDatabase"
+POSTGREUSER = "myUser"
+POSTGREPASS = "myPass"
+postgredb = psycopg2.connect(database=POSTGREDATABASE, user=POSTGREUSER, password=POSTGREPASS)
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+@app.route("/mongo")
+def mongo():
+    cursor = mongodb[MONGOCOLLECTION].find()
+    docs = []
+    for doc in cursor:
+    	docs.append(doc)
+
+    return str(docs)
+
+@app.route("/postgre")
+def postgre():
+    cursor = postgredb.cursor()
+    cursor.execute("SELECT * FROM table")
+
+    return str(cursor.fetchall())   
+
+if __name__ == "__main__":
+    app.run()
