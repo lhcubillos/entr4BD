a02b39fe917d13809e9acf72f63dde13527fe089
Geraldine Monsalve <gnmonsalve@uc.cl>
2016-08-07 18:31:15 -0400

    :star: :sparkles: completed queries form

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -3,11 +3,18 @@ import sys
 import psycopg2
 import pymongo
 from pymongo import MongoClient
+from flask_bootstrap import Bootstrap
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+# app = Flask(__name__)
+def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
 
-app = Flask(__name__)
+  return app
+
+app = create_app()
 
 
 MONGODATABASE = "myDatabase"
@@ -17,30 +24,48 @@ MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
 mongodb = client[MONGODATABASE]
 
-POSTGREDATABASE = "mydatabase"
-POSTGREUSER = "myuser"
-POSTGREPASS = "myPass"
-postgredb = psycopg2.connect(database=POSTGREDATABASE, user=POSTGREUSER, password=POSTGREPASS)
+POSTGRESDATABASE = "mydatabase"
+POSTGRESUSER = "myuser"
+POSTGRESPASS = "myPass"
+postgresdb = psycopg2.connect(database=POSTGRESDATABASE, user=POSTGRESUSER, password=POSTGRESPASS)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Hello World!"
+    if request.method == 'POST':
+        query = request.form['query']
+        db = request.form['db']
+        if db == 'postgres':
+            return postgres(query)
+        else:
+            return mongo(query)
+    else:
+        return render_template('form.html')
+
 
 @app.route("/mongo")
-def mongo():
-    cursor = mongodb[MONGOCOLLECTION].find()
-    docs = []
-    for doc in cursor:
-    	docs.append(doc)
+def mongo(query):
+    # cursor = mongodb[MONGOCOLLECTION].find()
+    # mongodb["myCollection"].find()
+    cursor = []
+    if "find" in query:
+        exec "cursor = "+query
+        docs = []
+        for doc in cursor:
+            docs.append(doc)
+        return str(docs)
+    else:
+        # mongodb['myCollection'].insert({'name': 'U2'})
+        exec query
+        return "ok"
 
-    return str(docs)
 
 
-@app.route("/postgre")
-def postgre():
-    cursor = postgredb.cursor()
-    cursor.execute("SELECT * FROM mytable;")
+@app.route("/postgres")
+def postgres(query):
+    cursor = postgresdb.cursor()
+    # cursor.execute("SELECT * FROM mytable;")
+    cursor.execute(query)
 
     return str(cursor.fetchall())
 
