f4b1df0ab88e80706ab99f45a002c4ce44efcbb0
mf <mfsepulveda@uc.cl>
2016-10-29 20:57:40 -0300

    Refactored

--- flaskr/__main__.py
+++ flaskr/__main__.py
@@ -1,46 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
 import os
 import sys
-import psycopg2
-import pymongo
+# import psycopg2
 import json
 from bson import json_util
-
 from pymongo import MongoClient
-# from flask_bootstrap import Bootstrap
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 def create_app():
-  app = Flask(__name__)
-  return app
+    app = Flask(__name__)
+    return app
 
 app = create_app()
 
-# Mongo config
+# REPLACE WITH YOUR DATABASE NAME
 MONGODATABASE = "myDatabase"
-# MONGOCOLLECTION = "myCollection"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
 mongodb = client[MONGODATABASE]
 
-# Postgres config
+''' # Uncomment for postgres connection
+# REPLACE WITH YOUR DATABASE NAME, USER AND PASS
 POSTGRESDATABASE = "mydatabase"
 POSTGRESUSER = "myuser"
 POSTGRESPASS = "mypass"
-postgresdb = psycopg2.connect(database=POSTGRESDATABASE, user=POSTGRESUSER, password=POSTGRESPASS)
+postgresdb = psycopg2.connect(
+    database=POSTGRESDATABASE,
+    user=POSTGRESUSER,
+    password=POSTGRESPASS)
+'''
 
 QUERIES_FILENAME = 'queries'
+
 
 @app.route("/")
 def home():
     with open(QUERIES_FILENAME, 'r') as queries_file:
         json_file = json.load(queries_file)
-        pairs = [(x["name"], x["database"], x["description"], x["query"]) for x in json_file]
-        return render_template('file.html', results = pairs)
+        pairs = [(x["name"],
+                  x["database"],
+                  x["description"],
+                  x["query"]) for x in json_file]
+        return render_template('file.html', results=pairs)
+
 
 @app.route("/mongo")
 def mongo():
@@ -52,19 +58,21 @@ def mongo():
     else:
         return "ok"
 
+
 @app.route("/postgres")
 def postgres():
     query = request.args.get("query")
     cursor = postgresdb.cursor()
     cursor.execute(query)
     results = [[a for a in result] for result in cursor]
-    print (results)
+    print(results)
     return render_template('postgres.html', results=results)
+
 
 @app.route("/example")
 def example():
     return render_template('example.html')
 
+
 if __name__ == "__main__":
-    app.debug = True
     app.run()
